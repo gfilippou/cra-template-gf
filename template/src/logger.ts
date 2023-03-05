1ffa095c93d0a40f4d6520fd3dd49ad9374643b7
George <gfilippou@gmail.com>
2023-03-06 01:03:22 +0200

    fix error logger

--- template/src/logger.ts
+++ template/src/logger.ts
@@ -2,8 +2,8 @@
 // This component would import and use an external error tracking service, e.g. Sentry
 
 export const logger = {
-  error: (message: string, error: Error, { ...rest }) => {
-    console.error(message, error, '\nAdditional info', rest);
+  error: (message: string, error: any, ...rest: any[]) => {
+    console.error(message, error, '\nAdditional info', ...rest);
     // logToExternalService({ message, error });
   },
 };
